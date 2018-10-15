ad84adb4827566cb1364668e4f163d6e9be4d2bd
Alexander Miller <alexanderm781@gmail.com>
2018-10-15 15:59:07 +1300

    Show accuracy chart in web view

--- www/index.php
+++ www/index.php
@@ -9,15 +9,16 @@
 			}
 		</style>
 	</head>
-
-<?php
-	function data_uri($file, $mime) {  
-  		$contents = file_get_contents($file);
-  		$base64   = base64_encode($contents); 
-  		return ('data:' . $mime . ';base64,' . $base64);
-	}
-?>
-<h1 style="margin: 50px 0px 0px 0px;">Microlensing Event Detection</h1>
-<img src="<?php echo data_uri('../nn.png','image/png'); ?>" alt="Network Status" style="margin: 0px auto;display:block;" />
-
+	<body>
+		<?php
+			function data_uri($file, $mime) {
+		  		$contents = file_get_contents($file);
+		  		$base64   = base64_encode($contents);
+		  		return ('data:' . $mime . ';base64,' . $base64);
+			}
+		?>
+		<h1 style="margin: 30px 0px 0px 0px;">Microlensing Event Detection</h1>
+		<img src="<?php echo data_uri('../nn.png','image/png'); ?>" alt="Network Status" style="margin: 15px auto;display:block;" />
+		<img src="<?php echo data_uri('../accuracy.png','image/png'); ?>" alt="Network Accuracy" style="margin: 15px auto;display:block;" />
+	</body>
 </html>
